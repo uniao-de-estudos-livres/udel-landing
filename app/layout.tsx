1a12da3a2c7f5e8618fbc3917001b8c49525c3f3
astahjmo <contato.johnatan06@gmail.com>
2025-02-22 22:19:15 -0400

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,14 @@ export const metadata: Metadata = {
   description: "Transforme seus estudos em uma jornada emocionante! Nosso site combina aprendizado e gamificação, oferecendo desafios interativos, conquistas e recompensas para tornar o estudo mais motivador e eficiente. Explore conteúdos, suba de nível e domine novos conhecimentos de forma divertida!",
   icons: [
     {
-      url: "../static/udel-white.svg"
+      url: "../public/udel-white.svg"
     }
   ],
   twitter: {
     title: "Udel - uma nova forma de estudar",
     description: "Transforme seus estudos em uma jornada emocionante! Nosso site combina aprendizado e gamificação, oferecendo desafios interativos, conquistas e recompensas para tornar o estudo mais motivador e eficiente. Explore conteúdos, suba de nível e domine novos conhecimentos de forma divertida!",
     creator: "@astahjmo",
-    images: ["../static/udel_-_natal.png"],
+    images: ["../public/udel_-_natal.png"],
     card: "summary_large_image"
   },
   openGraph: {
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     title: "Udel - uma nova forma de estudar",
     description: "Transforme seus estudos em uma jornada emocionante! Nosso site combina aprendizado e gamificação, oferecendo desafios interativos, conquistas e recompensas para tornar o estudo mais motivador e eficiente. Explore conteúdos, suba de nível e domine novos conhecimentos de forma divertida!",
     images: [
-      "../static/udel_-_natal.png"
+      "../public/udel_-_natal.png"
     ],
   }
 };
