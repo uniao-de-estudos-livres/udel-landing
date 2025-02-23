5563aa85d78ed1c3212a45b7144e1653555cb544
astahjmo <contato.johnatan06@gmail.com>
2025-02-22 22:16:11 -0400

    fix: logo size

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -39,7 +39,7 @@ export function HeroSection() {
                     </div>
                     {/* Logo */}
                     <div className="flex justify-center lg:justify-end">
-                        <Logo variant="white" />
+                        <Logo variant="white" className='w-full max-w-[200px] sm:max-w-[300px] lg:max-w-[400px]"' />
                     </div>
                 </div>
             </div>
